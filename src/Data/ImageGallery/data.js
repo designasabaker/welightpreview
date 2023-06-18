72a364c72f6ee70d09adc3ed2f0fb6e4e98894cc
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-06-18 01:19:30 -0400

    Add image gallery to the login page

--- src/Data/ImageGallery/data.js
+++ src/Data/ImageGallery/data.js
@@ -1,46 +1,51 @@
 import JackImg from "../../assets/images/Jack.jpg";
+import AndyImg from "../../assets/images/Andy.jpg";
+import JinImg from "../../assets/images/Jin.jpg";
+import GraceImg from "../../assets/images/Grace.jpg";
+import DriImg from "../../assets/images/Dri.jpg";
+import GeorgeImg from "../../assets/images/George.jpg";
 
 export const data = [
     {
         id: 1,
-        name: "Jack",
-        title: "CEO",
+        name: "Jack Wang",
+        title: "Founder, CEO",
         image: JackImg,
-        slogan: "I am Jack. we are the best team in the world.",
+        slogan: "I am Jack. we are the best team in the world. I am Jack. we are the best team in the world.",
     },
     {
         id: 2,
         name:"Andy",
         title: "CEO",
-        image: JackImg,
-        slogan: "I am Jack. we are the best team in the world.",
+        image: AndyImg,
+        slogan: "I am Andy. we are the best.",
     },
     {
         id: 3,
         name:"Jin",
         title: "CTO",
-        image: JackImg,
+        image: JinImg,
         slogan: "I am Jack. we are the best team in the world.",
     },
     {
         id: 4,
         name:"Grace",
         title: "CFO",
-        image: JackImg,
+        image: GraceImg,
         slogan: "I am Jack. we are the best team in the world.",
     },
     {
         id: 5,
         name:"Dri",
         title: "COO",
-        image: JackImg,
+        image: DriImg,
         slogan: "I am Jack. we are the best team in the world.",
     },
     {
         id: 6,
         name:"George",
         title: "CIO",
-        image: JackImg,
+        image: GeorgeImg,
         slogan: "I am Jack. we are the best team in the world.",
     }
 ]
--- src/components/ImageGallery/GalleryPrevBtn.js
+++ src/components/ImageGallery/GalleryPrevBtn.js
@@ -1,3 +1,6 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleChevronLeft} from '@fortawesome/free-solid-svg-icons'
+
 export const GalleryPrevBtn = (props) => {
     const {onClick} = props;
 
@@ -5,7 +8,7 @@ export const GalleryPrevBtn = (props) => {
         <button
             onClick={onClick}
         >
-            Prev
+            <FontAwesomeIcon icon={faCircleChevronLeft} style={{color: "#ffffff",}} />
         </button>
     )
 }
