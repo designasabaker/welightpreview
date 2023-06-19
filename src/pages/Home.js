a1338d1d1767c68da7a281ecd9ed7158877db640
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-06-18 20:13:14 -0400

    Updating Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,25 +2,32 @@ import React, {useContext} from "react";
 import {Link} from "react-router-dom";
 import {AppContext} from "../context/appContext";
 import { motion } from "framer-motion"
+import bgImage from '../assets/images/background/bg.png';
 
 export default function Home() {
   const {hasLoggedIn} = useContext(AppContext);
   return (
-    <motion.div
-        initial={{ opacity: 0, y: 100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        className="flex flex-col justify-center items-center h-[75vh]">
-      <h1>WeLight, 留学申请的夜路上多一盏灯</h1>
-      <h1>We light the future.</h1>
-      <button
-        // variant="outlined"
-        className={"w-36 my-6 font-sans block my-1 bg-transparent hover:bg-blue-500 text-blue-500 " +
-            "font-semibold hover:text-white py-2 px-4 " +
-            "border border-blue-500 hover:border-transparent rounded"}
-      >
-        {hasLoggedIn ? <Link to="/Profile/basic-info">Go to My Profile</Link> : <Link to="/login">Login</Link>}
-      </button>
-    </motion.div>
+      <>
+        <div style={{ backgroundImage: `url(${bgImage})` }}>
+          <motion.div
+            className="flex flex-col px-16 h-screen"
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+              <div className={"flex-grow"} /> {/* This is a spacer */}
+              <h1 className={"text-white text-6xl py-3"}>WELIGHT TECH</h1>
+              <p className={"text-white pl-2"}>WeLight 留学申请的夜路上多一盏灯</p>
+              <p className={"text-white pl-2 pb-3"}>We light the future.</p>
+              <Link
+                // variant="outlined"
+                className={"w-36 mt-3 mb-24 ml-2 py-2 px-4 font-sans block my-1 bg-transparent hover:bg-blue-500 text-blue-500 text-center border border-blue-500 rounded hover:text-white hover:border-transparent rounded"}
+                to={hasLoggedIn ? "/Profile" : "/login"}
+              >
+                {hasLoggedIn ? "Go to My Profile" : "Login"}
+              </Link>
+          </motion.div>
+        </div>
+      </>
   );
 }
