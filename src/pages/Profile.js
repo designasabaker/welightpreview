3c392347f491bf541dd1dd13563dc5b4e1a3fcaf
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-06-28 22:49:43 -0400

    Fix login page input box style

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,7 +6,7 @@ function Profile(){
     const navigate = useNavigate();
 
     return(
-        <div className={"flex flex-col pr-36"}>
+        <div className={"flex flex-col pr-3"}>
             <h1 className={titleStyle}>Welcome!</h1>
             <p className={"font-sans font-thin"}>We're excited to announce a new feature that is set to significantly enhance your experience on our platform. We have introduced a User Profile section, a personal space where you can provide and manage your information to tailor your experiences and interactions on our platform.<br/><br/>
 
@@ -16,7 +16,7 @@ function Profile(){
 
                 Thank you for being a valued member of our community!</p>
             <button
-                className={`w-full h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold my-6 py-2 px-4 rounded`}
+                className={`h-12 bg-blue-500 hover:bg-blue-700 text-white font-bold my-6 py-2 px-4 rounded`}
                 onClick={()=>{
                     navigate("/profile/basic-info");
                 }}>
