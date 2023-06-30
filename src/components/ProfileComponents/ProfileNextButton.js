a655ceaff76be95f42a38bf62b18cf2b6c116d91
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-06-30 17:53:07 -0400

    add required check in profile page

--- src/components/ProfileComponents/ProfileNextButton.js
+++ src/components/ProfileComponents/ProfileNextButton.js
@@ -3,21 +3,25 @@ import {useContext} from "react";
 import {ProfileContext} from "../../context/profileContext";
 
 export default function ProfileNextButton(props) {
-    const {isActive, partParam} = props;
+    const {isActive, partParam, canShowAlert, isRequiredFilled, setCanShowAlert, checkRequiredFilled} = props;
     const navigate = useNavigate();
     const {nextProfileComponentPath} = useContext(ProfileContext);
 
     // console.log("nextBtn isActive", isActive);
 
     return (
-        <button
-            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${isActive ? "" : "opacity-50 cursor-not-allowed"}`}
-            onClick={()=>{
-            if(isActive) {
-                navigate(nextProfileComponentPath(partParam));
-            }
-        }}>
-            Next
-        </button>
+        <div className={"flex flex-row items-center"}>
+            {(canShowAlert && !isRequiredFilled) && <div className={"text-red-500 text-sm mx-6"}>Please fill in the required fields</div>}
+            <button
+                className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${isActive ? "" : "opacity-50 cursor-not-allowed"}`}
+                onClick={()=>{
+                    setCanShowAlert(true);
+                    if(isActive && isRequiredFilled){
+                        navigate(nextProfileComponentPath(partParam));
+                    }
+            }}>
+                Next
+            </button>
+        </div>
     )
 }
