0e26aba9e0275586a50fa5e752b137ada76047cc
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-07-10 14:30:11 -0400

    Fix svg overflow

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
@@ -7,6 +8,14 @@ const Footer = () => {
         <footer className="bg-transparent text-black py-4 px-6">
             <div className="container mx-auto text-center">
                 <p>&copy; {currentYear} Welight Tech. All rights reserved.</p>
+                <a
+                    href="https://www.linkedin.com/company/welight-tech/mycompany/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-white hover:text-gray-500"
+                >
+                    <FaLinkedin size={24} />
+                </a>
             </div>
         </footer>
     );
